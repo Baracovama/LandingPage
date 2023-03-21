4322ce3b583cea0f7d5f36f219585bb13ef32172
Baracovama <juanvguevaram@gmail.com>
2023-03-21 22:13:24 +0000

    Proyecto finalizado

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer = () => {
+	return (
+		<div>
+			<div className=" text-center p-2 mt-2 bg-secondary text-white bg-dark">
+				<p className="text pt-3">Copyright © Your website 2022</p>
+			</div>
+		</div>
+	);
+};
+export default Footer;
